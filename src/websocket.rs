d3b6c6bc293dc8189754decf6d0e535f96577e3f
Jibao Mansaray <jibaomansaray@gmail.com>
2023-11-27 22:26:08 +1000

    Use the new websocket server and session implementations

--- src/websocket.rs
+++ src/websocket.rs
@@ -1,38 +1,26 @@
-use actix::{Actor, AsyncContext, StreamHandler};
+use std::time::Instant;
+
+use actix::Addr;
 use actix_web::{web, Error, HttpRequest, HttpResponse};
 use actix_web_actors::ws;
 
-/// Define HTTP actor
-///
-#[derive(Default)]
-struct PartyCrasherWs;
-
-impl Actor for PartyCrasherWs {
-    type Context = ws::WebsocketContext<Self>;
-}
-
-/// Handler for ws::Message message
-impl StreamHandler<Result<ws::Message, ws::ProtocolError>> for PartyCrasherWs {
-    fn handle(&mut self, msg: Result<ws::Message, ws::ProtocolError>, ctx: &mut Self::Context) {
-        match msg {
-            Ok(ws::Message::Ping(msg)) => ctx.pong(&msg),
-            Ok(ws::Message::Text(text)) => ctx.text(text),
-            Ok(ws::Message::Binary(bin)) => ctx.binary(bin),
-            _ => (),
-        }
-    }
-
-    fn started(&mut self, ctx: &mut Self::Context) {
-        ctx.text("welcome to the connection");
-        ctx.text("welcome to the connection2");
-    }
-}
+pub(crate) mod server;
+pub(crate) mod session;
 
 pub(crate) async fn handle_websocket(
     req: HttpRequest,
     stream: web::Payload,
+    srv: web::Data<Addr<server::ChatServer>>,
 ) -> Result<HttpResponse, Error> {
-    let resp = ws::start(PartyCrasherWs::default(), &req, stream);
-    println!("{:?}", resp);
-    resp
+    ws::start(
+        session::WsChatSession {
+            id: 0,
+            hb: Instant::now(),
+            room: "main".to_owned(),
+            name: None,
+            addr: srv.get_ref().clone(),
+        },
+        &req,
+        stream,
+    )
 }
