c6793176ab48fa7927672101af936109e2a110e8
Jibao Mansaray <jibaomansaray@gmail.com>
2023-11-27 23:48:21 +1000

    Fix: Check if websocket is enabled

--- src/websocket.rs
+++ src/websocket.rs
@@ -4,6 +4,8 @@ use actix::Addr;
 use actix_web::{web, Error, HttpRequest, HttpResponse};
 use actix_web_actors::ws;
 
+use crate::config::Config;
+
 pub(crate) mod server;
 pub(crate) mod session;
 
@@ -11,7 +13,12 @@ pub(crate) async fn handle_websocket(
     req: HttpRequest,
     stream: web::Payload,
     srv: web::Data<Addr<server::ChatServer>>,
+    config: web::Data<Config>,
 ) -> Result<HttpResponse, Error> {
+    if !config.is_ws_enabled() {
+        return Ok(HttpResponse::NotAcceptable().body("websocket not enabled"));
+    }
+
     ws::start(
         session::WsChatSession {
             id: 0,
