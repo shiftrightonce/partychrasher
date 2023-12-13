22def82fd4c18a40d64550e08796507f3c991659
Jibao Mansaray <jibaomansaray@gmail.com>
2023-12-14 09:47:09 +1000

    Register album and artist routes

--- src/web_app/api/v1.rs
+++ src/web_app/api/v1.rs
@@ -2,6 +2,8 @@ use actix_web::web;
 
 use crate::web_app::auth_middleware;
 
+mod v1_album;
+mod v1_artist;
 mod v1_client;
 mod v1_file_server;
 mod v1_track;
@@ -13,6 +15,10 @@ pub(crate) fn config_api_service(config: &mut web::ServiceConfig) {
     api_routes = v1_client::register_routes(api_routes);
     // track routes
     api_routes = v1_track::register_routes(api_routes);
+    // album routes
+    api_routes = v1_album::register_routes(api_routes);
+    // artist routes
+    api_routes = v1_artist::register_routes(api_routes);
     // file stream routes
     api_routes = v1_file_server::register_routes(api_routes);
 
