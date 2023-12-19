fea1cfc3a7da7b6b21f47d7f5dc37f7a4ad35543
Jibao Mansaray <jibaomansaray@gmail.com>
2023-12-19 17:13:57 +1000

    Ignore duplicates

--- src/entity/album_artist/album_artist_repo.rs
+++ src/entity/album_artist/album_artist_repo.rs
@@ -35,7 +35,7 @@ CREATE TABLE "album_artists" (
     }
 
     pub(crate) async fn create(&self, entity: InAlbumArtistEntityDto) -> Option<AlbumArtistEntity> {
-        let sql = "INSERT INTO album_artists (album_id, artist_id) values (?, ?)";
+        let sql = "INSERT OR IGNORE INTO album_artists (album_id, artist_id) values (?, ?)";
         if let Err(e) = sqlx::query(sql)
             .bind(&entity.album_id)
             .bind(&entity.artist_id)
