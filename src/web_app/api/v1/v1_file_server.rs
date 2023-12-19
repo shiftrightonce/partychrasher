5f4cd1f6e75671a89dd0a74974de897a6e352605
Jibao Mansaray <jibaomansaray@gmail.com>
2023-12-19 17:20:22 +1000

    Add an `serve/{media_id}` endpoint for serving any media file

--- src/web_app/api/v1/v1_file_server.rs
+++ src/web_app/api/v1/v1_file_server.rs
@@ -2,13 +2,13 @@ use std::sync::Arc;
 
 use actix_web::{
     error::{ErrorNotFound, ErrorUnauthorized},
-    get, web, HttpRequest, Scope,
+    get, web, HttpRequest, Responder, Scope,
 };
 
 use crate::{db::DbManager, entity::client::OutClientEntityDto, web_app::when_user};
 
 pub(crate) fn register_routes(scope: Scope) -> Scope {
-    scope.service(serve)
+    scope.service(serve).service(serve_file)
 }
 
 #[get("/stream/{id}")]
@@ -30,6 +30,25 @@ pub(crate) async fn serve(
         Err(ErrorNotFound(format!(
             "file with ID: {:?} not found",
             the_id
+        )))
+    }
+}
+
+#[get("serve/{media_id}")]
+pub(crate) async fn serve_file(req: HttpRequest, media_id: web::Path<String>) -> impl Responder {
+    let (_, response) = when_user::<OutClientEntityDto>(&req).await;
+
+    if response.is_some() {
+        return Err(ErrorUnauthorized("Permission denied"));
+    }
+    let db_manager = req.app_data::<Arc<DbManager>>().unwrap();
+
+    if let Some(media) = db_manager.media_repo().find_by_id(media_id.as_str()).await {
+        actix_files::NamedFile::open(media.path).map_err(Into::into)
+    } else {
+        Err(ErrorNotFound(format!(
+            "file with ID: {:?} not found",
+            media_id
         )))
     }
 }
