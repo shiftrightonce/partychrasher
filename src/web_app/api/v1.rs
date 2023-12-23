b4e61b0a064a689e84bae2f12c194725b1ee9019
= <jibaomansaray@gmail.com>
2023-12-23 15:44:34 +1000

    Add player endpoint

--- src/web_app/api/v1.rs
+++ src/web_app/api/v1.rs
@@ -6,6 +6,7 @@ mod v1_album;
 mod v1_artist;
 mod v1_client;
 mod v1_file_server;
+mod v1_player;
 mod v1_playlist;
 mod v1_search;
 mod v1_track;
@@ -29,10 +30,12 @@ pub(crate) fn config_api_service(config: &mut web::ServiceConfig) {
     api_routes = v1_playlist::register_routes(api_routes);
     // file stream routes
     api_routes = v1_file_server::register_routes(api_routes);
-    // Search route
+    // Search routes
     api_routes = v1_search::register_routes(api_routes);
-    // Websocket
+    // Websocket routes
     api_routes = v1_websocket::register_routes(api_routes);
+    // Player routes
+    api_routes = v1_player::register_routes(api_routes);
 
     config.service(
         api_routes
@@ -40,7 +43,7 @@ pub(crate) fn config_api_service(config: &mut web::ServiceConfig) {
             .wrap(auth_middleware::Auth),
     );
 
-    // -- Open routes
+    // -- Open routes: Routes that do not require authenticating
     // client open routes
     open_api_routes = v1_client::register_open_routes(open_api_routes);
 
